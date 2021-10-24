9fc7caaae365abbf4ea6b07b1c071ed49071575c
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2021-10-24 19:57:09 +0300

    init commit

---
+++ test_python/app.py
@@ -0,0 +1,12 @@
+import time
+print("multiple times print1")
+time.sleep(1)
+print("multiple times print2")
+time.sleep(1)
+print("multiple times print3")
+time.sleep(1)
+print("multiple times print4")
+time.sleep(1)
+print("multiple times print5")
+time.sleep(1)
+print("multiple times print6")
